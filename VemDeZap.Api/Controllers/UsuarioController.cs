0ac49f8c46533463da0360818a47a21518b5df97
Giovani da Silva <giovani.silva@govbr.com.br>
2021-02-11 13:51:26 -0300

    Adicionado comando de autenticação

--- VemDeZap.Api/Controllers/UsuarioController.cs
+++ VemDeZap.Api/Controllers/UsuarioController.cs
@@ -1,10 +1,17 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
 using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Threading;
 using System.Threading.Tasks;
+using VemDeZap.Api.Security;
 using VemDeZap.Domain.Commands.Usuario.AdicionarUsuario;
+using VemDeZap.Domain.Commands.Usuario.AutenticarUsuario;
 using VemDeZap.Infra.Repositories.Transactions;
 
 namespace VemDeZap.Api.Controllers
@@ -64,7 +71,7 @@ namespace VemDeZap.Api.Controllers
 
         [AllowAnonymous]
         [HttpGet]
-        [Route("Versao")]
+        [Route("api/Usuario/Versao")]
         public string Versao()
         {            
          return "VemDeZap v. 1.0.0";            
@@ -72,7 +79,7 @@ namespace VemDeZap.Api.Controllers
 
         [AllowAnonymous]
         [HttpPost]
-        [Route("Adicionar")]
+        [Route("api/Usuario/Adicionar")]
         public async Task<IActionResult> Adicionar([FromBody] AdicionarUsuarioRequest request)
         {
             try
@@ -84,6 +91,83 @@ namespace VemDeZap.Api.Controllers
             catch (Exception ex)
             {
                 return BadRequest(ex.Message);
+            }
+        }
+
+
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("api/Usuario/Autenticar")]
+        public async Task<IActionResult> Autenticar(
+           [FromBody] AutenticarUsuarioResquest request,
+           [FromServices] SigningConfigurations signingConfigurations,
+           [FromServices] TokenConfigurations tokenConfigurations)
+        {
+
+            try
+            {
+                var autenticarUsuarioResponse = await _mediator.Send(request, CancellationToken.None);
+
+                if (autenticarUsuarioResponse.Autenticado == true)
+                {
+                    var response = GerarToken(autenticarUsuarioResponse, signingConfigurations, tokenConfigurations);
+
+                    return Ok(response);
+                }
+
+                return Ok(autenticarUsuarioResponse);
+
+            }
+            catch (System.Exception ex)
+            {
+
+                return NotFound(ex.Message);
+            }
+        }
+
+
+        private object GerarToken(AutenticarUsuarioResponse response, SigningConfigurations signingConfigurations, TokenConfigurations tokenConfigurations)
+        {
+            if (response.Autenticado == true)
+            {
+                ClaimsIdentity identity = new ClaimsIdentity(
+                    new GenericIdentity(response.Id.ToString(), "Id"),
+                    new[] {
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString("N")),
+                        //new Claim(JwtRegisteredClaimNames.UniqueName, response.Usuario)
+                        new Claim("Usuario", JsonConvert.SerializeObject(response))
+                    }
+                );
+
+                DateTime dataCriacao = DateTime.Now;
+                DateTime dataExpiracao = dataCriacao + TimeSpan.FromSeconds(tokenConfigurations.Seconds);
+
+                var handler = new JwtSecurityTokenHandler();
+                var securityToken = handler.CreateToken(new SecurityTokenDescriptor
+                {
+                    Issuer = tokenConfigurations.Issuer,
+                    Audience = tokenConfigurations.Audience,
+                    SigningCredentials = signingConfigurations.SigningCredentials,
+                    Subject = identity,
+                    NotBefore = dataCriacao,
+                    Expires = dataExpiracao
+                });
+                var token = handler.WriteToken(securityToken);
+
+                return new
+                {
+                    authenticated = true,
+                    created = dataCriacao.ToString("yyyy-MM-dd HH:mm:ss"),
+                    expiration = dataExpiracao.ToString("yyyy-MM-dd HH:mm:ss"),
+                    accessToken = token,
+                    message = "OK",
+                    PrimeiroNome = response.Nome
+                };
+            }
+            else
+            {
+                return response;
             }
         }
     }
